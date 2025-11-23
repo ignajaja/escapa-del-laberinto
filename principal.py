3a3727893f738a3a36dd266bac982e50dd829ac0
ignacio elizondo <you@example.com>
2025-11-22 22:21:18 -0600

    funciones-principales

--- principal.py
+++ principal.py
@@ -8,20 +8,34 @@ ALTO_VEN = 600
 TAMANO_ESPACIO = 40
 
 class Puntajes:
-    def __init__(self, nombre, pantalla):
-        self.nombre = nombre
-
-    def ordenar(self):
-        for i in range(len(self.puntajes)):
-            for j in range(i+1, len(self.puntajes)):
-                if self.puntajes[j]['puntaje'] > self.puntajes[i]['puntaje']:
-                    self.puntajes[i], self.puntajes[j] = self.puntajes[j], self.puntajes[i]
-
+    def __init__(self):
+        self.archivo = "puntajes.json"
+        self.puntajes = []
+        self.cargar_punt()
     
-    def guardar(self):
-        with open('puntajes.json', 'w') as a:
-            
-            json.dump(self.puntajes, a)
+    def cargar_punt(self):
+        try:
+            with open(self.archivo, 'r', encoding='utf-8') as a:
+                self.puntajes = json.load(a)
+        except:
+            messagebox.showerror("Error", "Error al cargar los puntajes")
+    
+    def guardar_punt(self):
+        try:
+            with open(self.archivo, 'w') as a:
+                    json.dump(self.puntajes, a, indent=4)
+        except:
+            messagebox.showerror("Error", "Error al guardar puntajes")
+
+    def agregar_punt(self, nombre, puntaje, tipo):
+        nuevo = {
+            "nombre": nombre,
+            "puntaje": puntaje,
+            "tipo": tipo
+        }
+        self.puntajes.append(nuevo)
+        self.puntajes.sort(key=lambda x: x['puntaje'], reverse=True)
+        self.guardar_punt()
 
 
 class Espacio:
@@ -39,6 +53,11 @@ class Espacio:
     def dibujar(self, pantalla):
         pygame.draw.rect(pantalla, self.color, (self.x * TAMANO_ESPACIO, self.y * TAMANO_ESPACIO, TAMANO_ESPACIO, TAMANO_ESPACIO))
         pygame.draw.rect(pantalla, "#000000", (self.x * TAMANO_ESPACIO, self.y * TAMANO_ESPACIO, TAMANO_ESPACIO, TAMANO_ESPACIO), 1)
+
+class Salida(Espacio):
+    def __init__(self, x, y):
+        super().__init__(x,y)
+        self.color = "#002fff"
 
 class Camino(Espacio):
     def __init__(self, x, y):
@@ -74,10 +93,9 @@ class Jugador:
     def __init__(self, x, y, nombre):
         self.x = x
         self.y = y
+        self.nombre = nombre
         self.energia = 100
         self.corriendo = False
-        self.vel = 2
-        self.vel_corriendo = 3
     
     def mover(self, mx, my, mapa):
         px = self.x + mx # px es la posición en la que va a quedar x
@@ -141,38 +159,36 @@ class Mapa:
         self.ancho = ancho
         self.alto = alto
         self.espacios = []
+        self.salida = (ancho-2, alto-2)
         self.generar_mapa()
 
     def generar_mapa(self):
         for y in range(self.alto):
             fila = []
             for x in range(self.ancho):
-                terreno = random.randint(0, 10)
-                fila.append(terreno)
+                terreno = random.randint(1,13)
+                if (x,y) == self.salida:
+                    fila.append(Salida(x,y))
+                
+                if x == 0 or x == self.ancho -1 or y == 0 or y == self.alto-1:
+                    fila.append(Muro(x,y))
+                
+                elif terreno == 1:
+                    fila.append(Muro(x,y))
+                elif terreno == 2:
+                    fila.append(Liana(x,y))
+                elif terreno == 3:
+                    fila.append(Tunel(x, y))
+                else:
+                    fila.append(Camino(x, y))
+
             self.espacios.append(fila)
-
-        # self.crear_terreno()
-
-        # self.asegurar_camino()
 
     def jugador_pasa(self, x, y):
         if 0 <= x < self.ancho and 0 <= y < self.alto:
             terreno = self.obtener_terreno(x,y)
             return terreno.jugador_pasa()
         return False
-            #     if x == 0 or x == self.ancho -1 or y == 0 or y == self.alto-1:
-            #         fila.append(Muro(x,y))
-                
-            #     elif terreno == 1:
-            #         fila.append(Muro(x,y))
-            #     elif terreno == 2:
-            #         fila.append(Liana(x,y))
-            #     elif terreno == 3:
-            #         fila.append(Tunel(x, y))
-            #     else:
-            #         fila.append(Camino(x, y))
-
-            # self.espacios.append(fila)
 
     def jugador_pasa(self, x, y):
         if 0<=x < self.ancho and 0<=y < self.alto:
@@ -186,22 +202,47 @@ class Mapa:
 
 
 class Juego:
-    def __init__(self, nombre):
+    def __init__(self, nombre, tipo):
         pygame.init()
         self.pantalla = pygame.display.set_mode((ANCHO_VEN, ALTO_VEN))
         self.reloj = pygame.time.Clock()
         self.running = True
-        self.nombre = nombre
-
+        self.jugador = Jugador(1,1,nombre)
+        self.tipo = tipo
+        self.tiempo_ini = pygame.time.get_ticks()
+        self.tiempo = 0
         self.mapa = Mapa(20,15)
-        self.jugador = Jugador(1,1, nombre)
+        self.juego_termi = False
+        self.juego_ganad = False
+        self.puntajes = Puntajes()
+    
+        pygame.display.set_caption("Escapa del laberinto")
+
+    def verificar_salida(self):
+        if (self.jugador.x, self.jugador.y) == self.mapa.salida:
+            return True
+        return False
+    
+    def calcular_puntaje(self):
+        return 1000 - (self.tiempo/1000)
+
+    def finalizar(self):
+        self.juego_termi = True
+        self.juego_ganad = True
+
+        segundos = self.tiempo/1000
+        puntaje = self.calcular_puntaje()
+        self.puntajes.agregar_punt(self.jugador.nombre, puntaje, self.tipo)
 
     def cerrar(self):
         for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
+            if evento.type ==pygame.QUIT:
                 self.running = False
 
     def actualizar(self):
+        if self.juego_termi:
+            return
+        self.tiempo = pygame.time.get_ticks() - self.tiempo_ini
 
         entradas = pygame.key.get_pressed()
         self.jugador.corriendo = entradas[pygame.K_LSHIFT]
@@ -218,14 +259,30 @@ class Juego:
             
         if not self.jugador.corriendo and self.jugador.energia < 100:
             self.jugador.energia += 0.5
+        if self.verificar_salida():
+            self.finalizar()
         
 
     def dibujar(self):
         self.pantalla.fill("#000000")
         self.mapa.dibujar(self.pantalla)
         self.jugador.dibujar(self.pantalla)
+        self.texto_pygame = pygame.font.Font(None, 28)
 
         pygame.draw.rect(self.pantalla, "#ff0000", (10,10, int(self.jugador.energia*2), 20))
+
+        if self.juego_termi and self.juego_ganad:
+            pantalla_victoria = pygame.Surface((ANCHO_VEN, ALTO_VEN))
+            pantalla_victoria.fill("#000000")
+            self.pantalla.blit(pantalla_victoria,(0,0))
+
+            texto_victoria = self.texto_pygame.render("Victoria", True, "#fffb00")
+            self.pantalla.blit(texto_victoria, (ANCHO_VEN//2, ALTO_VEN//2))
+
+            puntaje = self.calcular_puntaje()
+            texto_puntaje = self.texto_pygame.render(f"{puntaje}", True, "#ffffff")
+            self.pantalla.blit(texto_puntaje, (ANCHO_VEN//2, ALTO_VEN//2-40))
+
 
         pygame.display.flip()
 
@@ -238,33 +295,6 @@ class Juego:
         
         pygame.quit()
 
-
-class Puntajes:
-    def __init__(self):
-        self.puntajes = []
-
-    def cargar_puntajes(self):
-        try:
-            with open('puntajes.json', 'r', encoding='utf-8') as a:
-                self.puntajes = json.load(a)
-        except:
-            messagebox.showerror("Error", "Error cargando puntajes")
-    
-    def guardar_puntajes(self):
-        try:
-            with open("puntajes.json", "w", encoding='utf-8') as a:
-                json.dump(self.puntajes, a, indent=4)
-        except:
-            messagebox.showerror("Error", "Error al guardar puntajes")
-    
-    def agregar_puntaje(self, nombre, puntaje):
-        punt = {
-            nombre: puntaje
-        }
-        self.puntajes.append(punt)
-        self.puntajes.sort(key=lambda x: x['puntaje'], reverse=True)
-        self.guardar_puntajes()
-
 class VentanaPrincipal:
     def __init__(self):
         self.nombre = None
@@ -272,16 +302,15 @@ class VentanaPrincipal:
         self.ventana.title("Escapa del laberinto")
         self.mostrar()
 
-    def iniciar_juego():
-        juego=Juego()
-        juego.ejecutar()
 
     def mostrar(self):
         tk.Label(self.ventana,text='Escapa del laberinto', font=("Arial", 18, "bold"),fg="#000000").pack(pady=20)
         tk.Label(self.ventana,text='Ingrese su nombre para iniciar:', font=("Arial", 15, "bold"), fg="#000000").pack(pady=10)
         self.entry_nombre = tk.Entry(self.ventana, font=("Arial", 14)); self.entry_nombre.pack(pady=10); self.entry_nombre.insert(0, 'Jugador')
-        self.entry_nombre.bind('<Return>', lambda event: VentanaPrincipal.iniciar_juego())
-        tk.Button(self.ventana, text='Iniciar Juego', font=("Arial", 14), command=VentanaPrincipal.iniciar_juego).pack(pady=20)
+        tk.Label(self.ventana,text='Ingrese 1 para jugar de escapista\ny 2 para cazador:', font=("Arial", 15, "bold"), fg="#000000").pack(pady=10)
+        self.entry_tipo = tk.Entry(self.ventana, font=("Arial", 14)); self.entry_tipo.pack(pady=10); self.entry_tipo.insert(0, '1')
+        
+        tk.Button(self.ventana, text='Iniciar Juego', font=("Arial", 14), command=self.iniciar_juego).pack(pady=20)
 
     def validar_nombre(self):
         nombre = self.entry_nombre.get().strip()
@@ -290,30 +319,71 @@ class VentanaPrincipal:
             return False
         return True
     
+    def validar_tipo(self):
+        tipo = self.entry_tipo.get().strip()
+        if not (tipo == "1" or tipo == "2"):
+            messagebox.showwarning("Error", "El tipo debe ser 1 o 2")
+            return False
+        return True
+    
     def iniciar_juego(self):
-        if self.validar_nombre():
+        if self.validar_nombre() and self.validar_tipo():
             self.nombre = self.entry_nombre.get()
+            self.tipo = self.entry_tipo.get()
             self.ventana.destroy()
 
-    
-
     def mostrar_puntajes(self):
+        archivo_punt = Puntajes()
+        puntajes = archivo_punt.puntajes
+
+        if not puntajes:
+            return messagebox.showinfo("Puntajes", "No hay puntajes")
+        
+        ventana_puntajes = tk.Toplevel(self.ventana)
+        ventana_puntajes.title("Puntajes")
+        tk.Label("Puntajes más altos")
+        panel_izq = tk.Frame(ventana_puntajes).pack(side='left', fill='both', padx=(0,10))
+        panel_der = tk.Frame(ventana_puntajes).pack(side='right', fill='both', padx=(0,10))
+
+        texto_esc = tk.Text(panel_izq, font=("Arial", 10, "bold")); texto_esc.pack()
+        texto_caz = tk.Text(panel_der, font=("Arial", 10, "bold")); texto_caz.pack()
+
+        texto_esc.insert("Escapistas: \n")
+        count = 0
+        for i, puntaje in enumerate(puntajes):
+            if puntaje['tipo'] == "1" and count!=5:
+                texto_esc.insert(f"{i+1} - {puntaje['nombre']}: {[puntaje['puntaje']]}\n")
+                count += 1
+                break
+            break
+
+        texto_caz.insert("Cazadores: \n")
+        count = 0
+        for j, puntaje in enumerate(puntajes):
+            if puntaje['tipo'] == "2" and count!=5:
+                texto_caz.insert(f"{j+1} - {puntaje['nombre']}: {[puntaje['puntaje']]}\n")
+                count += 1
+                break
+            break
+
 
     def ejecutar(self):
         self.ventana.mainloop()
-        return self.nombre
+        return self.nombre, self.tipo
 
     
 def iniciar():
     ventana_principal = VentanaPrincipal()
-    nombre = ventana_principal.ejecutar()
+    nombre, tipo = ventana_principal.ejecutar()
 
     if nombre == None:
-        nombre = 'Jugador'
+        nombre = 'JUG'
+    if tipo == None:
+        tipo = "1"
 
-    juego = Juego(nombre)
+    juego = Juego(nombre, tipo)
     juego.ejecutar()
-    root = tk.TK()
+    root = tk.Tk()
     root.withdraw()
     root.destroy()
 
